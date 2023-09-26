0cfdff60e5ea98639741d34e1c0c4dca96029e57
Gustavo Baeza <gustavobaeza.2001@hotmail.com>
2023-09-26 11:48:25 -0300

    Ap07(jogo): Jogo Apostila 07, PSC

---
+++ Jogo.java
@@ -0,0 +1,9 @@
+public class Jogo {
+    public static void main (String[] args) {
+        Personagem cacador = new Personagem();
+        cacador.nome = "Jonh";
+        cacador.cacar();
+        cacador.comer();
+        cacador.dormir();        
+    }    
+}
